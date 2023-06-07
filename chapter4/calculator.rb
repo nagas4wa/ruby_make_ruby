0267a05cd1d96e8d13c6bb45fc196e103b460843
nagas4wa <masakinagasawa1122@gmail.com>
2023-06-07 22:50:54 +0900

    累乗と余剰のサポート

--- chapter4/calculator.rb
+++ chapter4/calculator.rb
@@ -20,6 +20,14 @@ def evaluate(tree)
     left = evaluate(tree[1])
     right = evaluate(tree[2])
     left / right
+  when "%"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left % right
+  when "**"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left ** right
   end
 end
 
