1cc58b9c8ca8ddd79a6e77bd74d564e98cc5f6e8
nagas4wa <masakinagasawa1122@gmail.com>
2023-06-07 22:55:38 +0900

    比較式のサポート

--- chapter4/calculator.rb
+++ chapter4/calculator.rb
@@ -28,6 +28,14 @@ def evaluate(tree)
     left = evaluate(tree[1])
     right = evaluate(tree[2])
     left ** right
+  when "=="
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left == right
+  when "<"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left < right
   end
 end
 
