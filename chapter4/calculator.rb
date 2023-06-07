4021951a483f010ac99c6ef4a92829b249b2ac2a
nagas4wa <masakinagasawa1122@gmail.com>
2023-06-07 22:48:56 +0900

    電卓の実装

---
+++ chapter4/calculator.rb
@@ -0,0 +1,32 @@
+require 'minruby'
+
+def evaluate(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left + right
+  when "-"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left - right
+  when "*"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left * right
+  when "/"
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left / right
+  end
+end
+
+str = gets
+
+tree = minruby_parse(str)
+
+answer = evaluate(tree)
+
+p answer
